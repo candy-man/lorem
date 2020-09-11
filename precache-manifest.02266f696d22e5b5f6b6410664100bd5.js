fcc68bd0dc545ae351fd4f89250a55acbe8abbb9
Aleksndar Kenderesi <alek.kenderesi@gmail.com>
2020-09-11 08:41:02 +0200

    f

---
+++ precache-manifest.02266f696d22e5b5f6b6410664100bd5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3312bf218eb5c48bfc5fd8ed5f1f864e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7553b8aa7b53cd440dca",
+    "url": "/static/css/2.14fbd4c8.chunk.css"
+  },
+  {
+    "revision": "21a36ec2083e09586b00",
+    "url": "/static/css/main.ad382f35.chunk.css"
+  },
+  {
+    "revision": "7553b8aa7b53cd440dca",
+    "url": "/static/js/2.065abdef.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.065abdef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21a36ec2083e09586b00",
+    "url": "/static/js/main.db310912.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "05aa77b14a3e3c5a79b60b00b34e9652",
+    "url": "/static/media/BalooTamma2-Regular.05aa77b1.ttf"
+  }
+]);
